d7bc7264dcb49c533a02ab29cabb44ce0810a693
Khoroshyj <sviatoslav.khoroshyi@lnu.edu.ua>
2026-05-14 10:37:11 +0300

    /

--- Lab_4/StartProgram.cpp
+++ Lab_4/StartProgram.cpp
@@ -1,4 +1,5 @@
 #include "StartProgram.h"
+#include "windows.h"
 #include "Drinks.h"
 #include "Alco.h"
 #include "NonAlco.h"
@@ -19,22 +20,21 @@ void StartProgram() {
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
 
-    Alco myVodka("Горілка", 500, 40.0);
-    NonAlco myJuice("Сік", 300, false);
-    Beer myBeer("Dunkell", 330, 4.5, true);
-    CocaCola myCoke("Coca-Cola", 500, true, false, "Vanilla");
-    Energy myEnergy("Red Bull", 250, true, 80);
     vector<Drinks*> drinkMenu;
-    drinkMenu.push_back(&myJuice);
-    drinkMenu.push_back(&myVodka);
-    drinkMenu.push_back(&myBeer);
-    drinkMenu.push_back(&myCoke);
-    drinkMenu.push_back(&myEnergy);
+    drinkMenu.push_back(new NonAlco("Сік", 300, false));
+    drinkMenu.push_back(new Alco("Горілка", 500, 40.0));
+    drinkMenu.push_back(new Beer("Dunkell", 330, 4.5, true));
+    drinkMenu.push_back(new CocaCola("Coca-Cola", 500, true, false, "Vanilla"));
+    drinkMenu.push_back(new Energy("Red Bull", 250, true, 80));
 
     for (size_t i = 0; i < drinkMenu.size(); ++i) {   
         cout << "=======[Напій №" << i + 1 << "]=======" << endl;
         drinkMenu[i]->Info();
         drinkMenu[i]->Recommend();
         cout << endl;
+    }
+
+    for (size_t i = 0; i < drinkMenu.size(); ++i) {
+        delete drinkMenu[i];
     }
 }
